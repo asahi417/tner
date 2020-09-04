123c36aa795fc17c4a2c3f96efd3bfd647a7c9d8
asahi417 <spacefunkspacefunk@gmail.com>
2020-09-04 14:58:08 +0900

    test script for dataset

--- test_dataset.py
+++ test_dataset.py
@@ -3,7 +3,11 @@ from src import get_dataset_ner
 
 if __name__ == '__main__':
 
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ontonote5')
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('conll_2003')
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('mit_movie_trivia')
     data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('mit_restaurant')
-    print(label_to_id, unseen_entity_set)
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('mit_movie_trivia', label_to_id=label_to_id)
-    print(label_to_id, unseen_entity_set)
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ner-cogent-en')
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ner-cogent-ja-mecab')
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('panx_dataset/en')
+    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('panx_dataset/ja')
