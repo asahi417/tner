1868e06d77675e29ad1f5005f85e87e618278c38
asahi417 <asahi1992ushio@gmail.com>
2020-09-22 19:44:01 +0900

    fix testing scripts

--- test_dataset.py
+++ test_dataset.py
@@ -2,12 +2,5 @@ from src import get_dataset_ner
 
 
 if __name__ == '__main__':
-
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ontonote5')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('conll_2003')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('mit_movie_trivia')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('mit_restaurant')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ner-cogent-en')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('ner-cogent-ja-mecab')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('panx_dataset/en')
-    data_split_all, label_to_id, language, unseen_entity_set = get_dataset_ner('panx_dataset/ja')
+    get_dataset_ner('ontonote5')
+    get_dataset_ner('conll_2003')
