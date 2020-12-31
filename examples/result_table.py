f4cafd770c3a7c59b7542d7b18bf70085028f65f
asahi417 <asahi1992ushio@gmail.com>
2020-12-31 17:38:10 +0000

    summary

--- examples/result_table.py
+++ examples/result_table.py
@@ -39,10 +39,10 @@ def summary(panx_multi_lingual: bool = False):
 
         for a in glob('{}/test*.json'.format(i)):
             test_data = a.split('test_')[-1].split('.json')[0]
-            # if panx_multi_lingual and test_data not in panx_data:
-            #     continue
-            # if not panx_multi_lingual and test_data not in data:
-            #     continue
+            if panx_multi_lingual and test_data not in panx_data:
+                continue
+            if not panx_multi_lingual and test_data not in all_data:
+                continue
 
             test_data = test_data.replace('-', '/')
             with open(a) as f:
