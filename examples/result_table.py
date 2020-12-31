bdc764621ce643a58d68454c70f88f4a96d133d8
asahi417 <asahi1992ushio@gmail.com>
2020-12-31 17:48:13 +0000

    summary

--- examples/result_table.py
+++ examples/result_table.py
@@ -93,10 +93,11 @@ def summary(panx_multi_lingual: bool = False):
                 continue
             tmp_out = dict_out_domain[metric][task]
             tmp_df = pd.DataFrame(tmp_out).T
-            tmp_df = tmp_df[data]
             if panx_multi_lingual:
+                tmp_df = tmp_df[data]
                 tmp_df = tmp_df.T[panx_data].T
             else:
+                tmp_df = tmp_df[all_data]
                 tmp_df = tmp_df.T[all_data].T
             tmp_df.to_csv('./ckpt/summary_out_domain_{}_{}{}.csv'.format(
                 task, metric, '_panx' if panx_multi_lingual else ''))
