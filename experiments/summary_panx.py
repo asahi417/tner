1cf7ce4bcb5c7779f338838159aa72e4f177958c
asahi417 <asahi1992ushio@gmail.com>
2021-01-06 23:23:05 +0000

    clean up code

--- experiments/summary_panx.py
+++ experiments/summary_panx.py
@@ -8,26 +8,21 @@ panx_data = ["panx_dataset/en", "panx_dataset/ja", "panx_dataset/ru", "panx_data
              "panx_dataset/ar"]
 
 
-def summary(base_model: bool = False):
+def summary():
     dict_in_domain = {'f1': {'es': {}, 'ner': {}}, 'recall': {'es': {}, 'ner': {}}, 'precision': {'es': {}, 'ner': {}}}
     dict_out_domain = {
         'f1': {'es': {}, 'ner': {}},
         'recall': {'es': {}, 'ner': {}},
         'precision': {'es': {}, 'ner': {}}
     }
-    if base_model:
-        checkpoint_dir = './ckpt/base'
-    else:
-        checkpoint_dir = './ckpt/large'
+    checkpoint_dir = './ckpt/model_large'
     for i in glob('{}/*'.format(checkpoint_dir)):
         if not os.path.isdir(i):
             continue
 
         with open('{}/parameter.json'.format(i)) as f:
             param = json.load(f)
-        if not base_model and param['transformers_model'] != "xlm-roberta-large":
-            continue
-        if base_model and param['transformers_model'] != "xlm-roberta-base":
+        if param['transformers_model'] != "xlm-roberta-large":
             continue
         if param['lower_case']:
             continue
@@ -44,19 +39,18 @@ def summary(base_model: bool = False):
                 continue
             test_data = test_data.replace('-', '/')
             test_data_raw = test_data.replace('_ignore', '').replace('_lower', '')
+            if 'ignore' in test_data:
+                continue
             if test_data_raw not in panx_data:
                 continue
 
             with open(a) as f:
                 test = json.load(f)
-            if 'test' in test.keys():
-                metric = test['test']
-            else:
-                metric = test['valid']
+            metric = test['test']
             f1 = round(metric['f1'], 2)
             recall = round(metric['recall'], 2)
             precision = round(metric['precision'], 2)
-            task = 'es' if 'ignore' in test_data else 'ner'
+            task = 'ner'
 
             if train_data not in dict_out_domain['f1'][task].keys():
                 dict_out_domain['f1'][task][train_data] = {}
@@ -74,25 +68,17 @@ def summary(base_model: bool = False):
     in_result = [list((dict_in_domain[c]['ner'].values())) for c in columns]
     in_result_key = list(dict_in_domain['f1']['ner'].keys())
     df = pd.DataFrame(in_result, columns=in_result_key, index=columns).T
-    if base_model:
-        df.to_csv('{}/summary_in_domain.panx.base.csv'.format(checkpoint_dir))
-    else:
-        df.to_csv('{}}/summary_in_domain.panx.csv'.format(checkpoint_dir))
+    df.to_csv('{}/summary_in_domain.panx.csv'.format(checkpoint_dir))
     pprint(df)
     for metric in ['f1', 'recall', 'precision']:
-        for task in ['es', 'ner']:
-            if task not in dict_out_domain[metric].keys():
-                continue
-            tmp_out = dict_out_domain[metric][task]
-            tmp_df = pd.DataFrame(tmp_out).T
-            pprint(tmp_df)
-            tmp_df = tmp_df[panx_data]
-            tmp_df = tmp_df.T[panx_data].T
-            if base_model:
-                tmp_df.to_csv('{}/summary_out_domain_{}_{}.panx.base.csv'.format(checkpoint_dir))
-            else:
-                tmp_df.to_csv('{}/summary_out_domain_{}_{}.panx.csv'.format(checkpoint_dir))
-            pprint(tmp_df)
+        task = 'ner'
+        tmp_out = dict_out_domain[metric][task]
+        tmp_df = pd.DataFrame(tmp_out).T
+        pprint(tmp_df)
+        tmp_df = tmp_df[panx_data]
+        tmp_df = tmp_df.T[panx_data].T
+        tmp_df.to_csv('{}/summary_out_domain.ner.panx.csv'.format(checkpoint_dir))
+        pprint(tmp_df)
 
 
 if __name__ == '__main__':
