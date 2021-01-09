3c54517429aa2f8d11d0e0844d8f546dcdff9797
asahi417 <asahi1992ushio@gmail.com>
2021-01-09 18:28:01 +0000

    fix other bugs and refactoring the code

--- experiments/clean.py
+++ experiments/clean.py
@@ -1,4 +1,19 @@
 import os
 from glob import glob
 
+ckpt = './ckpt'
+for i in glob('{}/*/*/*_ignore.json'):
+    os.rename(i, i.replace('_ignore', '_span'))
+
+# for i in glob('{}/*/*'):
+#     data_name = i.split('/')[-1]
+#     for _i in glob('{}/test*.json'.format(i)):
+#         if data_name in _i:
+#             continue
+#         if 'all_english' in _i:
+#             continue
+#
+#         if '_span' not in _i:
+#             os.remove(_i)
+
 
