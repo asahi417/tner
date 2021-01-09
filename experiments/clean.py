d1a889955cba20f9061c6c758f9eeb00ef9d80ea
asahi417 <asahi1992ushio@gmail.com>
2021-01-09 18:49:34 +0000

    fix other bugs and refactoring the code

--- experiments/clean.py
+++ experiments/clean.py
@@ -2,29 +2,29 @@ import os
 from glob import glob
 
 ckpt = './ckpt'
-for i in glob('{}/*/*/*_ignore.json'.format(ckpt)):
+for i in glob('{}/*/*/*_ignore*.json'.format(ckpt)):
     os.rename(i, i.replace('_ignore', '_span'))
 
-for i in glob('{}/*/*'.format(ckpt)):
-    if not os.path.isdir(i):
-        continue
-    data_name = i.split('/')[-1]
-    is_lower = '_lower' in i
-    print(i, is_lower, data_name)
-    input()
-    for _i in glob('{}/test*.json'.format(i)):
-        if data_name in _i:
-            continue
-        if 'all_english' in _i:
-            continue
-
-        if '_span' not in _i:
-            os.remove(_i)
-
-        if '_lower' not in _i and is_lower:
-            os.remove(_i)
-
-        if '_lower' in _i and not is_lower:
-            os.remove(_i)
+# for i in glob('{}/*/*'.format(ckpt)):
+#     if not os.path.isdir(i):
+#         continue
+#     data_name = i.split('/')[-1]
+#     is_lower = '_lower' in i
+#     print(i, is_lower, data_name)
+#     input()
+#     for _i in glob('{}/test*.json'.format(i)):
+#         if data_name in _i:
+#             continue
+#         if 'all_english' in _i:
+#             continue
+#
+#         if '_span' not in _i:
+#             os.remove(_i)
+#
+#         if '_lower' not in _i and is_lower:
+#             os.remove(_i)
+#
+#         if '_lower' in _i and not is_lower:
+#             os.remove(_i)
 
 
