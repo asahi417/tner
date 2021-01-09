b1c2db9f436a0cb5843948d919645db8b361826b
asahi417 <asahi1992ushio@gmail.com>
2021-01-09 19:11:13 +0000

    fix other bugs and refactoring the code

--- experiments/summary_baseline.py
+++ experiments/summary_baseline.py
@@ -5,32 +5,28 @@ from pprint import pprint
 from glob import glob
 
 data_ = ["ontonotes5", "conll2003",  "wnut2017", "panx_dataset/en", "bionlp2004", "bc5cdr", "fin"]
-data_lower = data_ + ["mit_restaurant", "mit_movie_trivia"]
+data_lower_ = data_ + ["mit_restaurant", "mit_movie_trivia"]
 
 
 def summary(base_model: bool = False, lower: bool = False):
-    dict_in_domain = {'f1': {'es': {}, 'ner': {}}, 'recall': {'es': {}, 'ner': {}}, 'precision': {'es': {}, 'ner': {}}}
-    dict_out_domain = {
-        'f1': {'es': {}, 'ner': {}},
-        'recall': {'es': {}, 'ner': {}},
-        'precision': {'es': {}, 'ner': {}}
-    }
+    f1_in_domain = {'es': {}, 'ner': {}}
+    f1_out_domain = {'es': {}, 'ner': {}}
+
     if lower:
-        data = data_lower
+        data = data_lower_
     else:
         data = data_
+    checkpoint_dir = './ckpt'
     if base_model:
-        if lower:
-            checkpoint_dir = './ckpt/model_base_lower'
-        else:
-            checkpoint_dir = './ckpt/model_base'
+        checkpoint_dir += '/model_base'
     else:
-        if lower:
-            checkpoint_dir = './ckpt/model_large_lower'
-        else:
-            checkpoint_dir = './ckpt/model_large'
+        checkpoint_dir += '/model_large'
 
-    print(glob('{}/*'.format(checkpoint_dir)))
+    if lower:
+        checkpoint_dir += 'lower'
+
+    print(checkpoint_dir)
+    input()
     for i in glob('{}/*'.format(checkpoint_dir)):
         if not os.path.isdir(i):
             continue
@@ -41,91 +37,64 @@ def summary(base_model: bool = False, lower: bool = False):
             continue
         if base_model and param['transformers_model'] != "xlm-roberta-base":
             continue
-        if lower:
-            if not param['lower_case']:
-                continue
-        else:
-            if param['lower_case']:
-                continue
+        if lower and not param['lower_case']:
+            continue
+        if not lower and param['lower_case']:
+            continue
         if len(param['dataset']) > 1:
             train_data = 'all_english'
         else:
             train_data = param['dataset'][0]
             if train_data not in data:
                 continue
-        print(i, train_data)
 
         for a in glob('{}/test*.json'.format(i)):
             test_data = a.split('test_')[-1].split('.json')[0]
-            if lower:
-                if 'lower' not in test_data:
-                    continue
-            else:
-                if 'lower' in test_data:
-                    continue
 
-            test_data = test_data.replace('-', '/')
-            test_data_raw = test_data.replace('_ignore', '').replace('_lower', '').replace('_span', '')
-            if test_data_raw not in data:
+            if lower and 'lower' not in test_data:
                 continue
+            if not lower and 'lower' in test_data:
+                continue
+            task = 'es' if 'ignore' in test_data or 'span' in test_data else 'ner'
+            test_data = test_data.replace('-', '/').replace('_lower', '').replace('_span', '')
+            print(train_data, test_data, task)
 
             with open(a) as f:
                 test = json.load(f)
-            if 'test' in test.keys():
-                metric = test['test']
-            else:
-                metric = test['valid']
+            metric = test['test'] if 'test' in test.keys() else test['valid']
             f1 = round(metric['f1'], 2)
-            recall = round(metric['recall'], 2)
-            precision = round(metric['precision'], 2)
-            task = 'es' if 'ignore' in test_data or 'span' in test_data else 'ner'
 
-            if train_data not in dict_out_domain['f1'][task].keys():
-                dict_out_domain['f1'][task][train_data] = {}
-                dict_out_domain['recall'][task][train_data] = {}
-                dict_out_domain['precision'][task][train_data] = {}
-            dict_out_domain['f1'][task][train_data][test_data_raw] = f1
-            dict_out_domain['recall'][task][train_data][test_data_raw] = recall
-            dict_out_domain['precision'][task][train_data][test_data_raw] = precision
-            if test_data == train_data:
-                dict_in_domain['f1'][task][test_data_raw] = f1
-                dict_in_domain['recall'][task][test_data_raw] = recall
-                dict_in_domain['precision'][task][test_data_raw] = precision
-        print(dict_out_domain)
-        input()
+            if train_data not in f1_out_domain[task].keys():
+                f1_out_domain[task][train_data] = {}
+            f1_out_domain[task][task][train_data][test_data_raw] = f1
+            if test_data == train_data and test_data not in f1_in_domain[task].keys():
+                f1_in_domain[task][test_data] = f1
 
-    pprint(dict_out_domain)
+    pprint(f1_in_domain)
+    pprint(f1_out_domain)
+
     if not lower:
-        columns = ['recall', 'precision', 'f1']
-        in_result = [list((dict_in_domain[c]['ner'].values())) for c in columns]
-        in_result_key = list(dict_in_domain['f1']['ner'].keys())
-        df = pd.DataFrame(in_result, columns=in_result_key, index=columns).T
-        df.to_csv('{}/summary_in_domain.csv'.format(checkpoint_dir))
+        in_result = [list(f1_in_domain['ner'].values())]
+        in_result_key = list(f1_in_domain['ner'].keys())
+        df = pd.DataFrame(in_result, columns=in_result_key, index=['f1']).T
         pprint(df)
-    for metric in ['f1', 'recall', 'precision']:
-        for task in ['es', 'ner']:
-            if task not in dict_out_domain[metric].keys():
-                continue
-            tmp_out = dict_out_domain[metric][task]
+        df.to_csv('{}/summary_in_domain.f1.csv'.format(checkpoint_dir))
 
-            tmp_df = pd.DataFrame(tmp_out).T
-            pprint(tmp_out)
-            print(metric, task)
-            pprint(tmp_df.columns)
-            pprint(tmp_df.T.columns)
-            input()
-            if task == 'es':
-                tmp_df = tmp_df[data]
-                all_data = data + ["all_english"]
-                tmp_df = tmp_df.T[all_data].T
-            else:
-                tmp_df = tmp_df.T['all_english']
-            tmp_df.to_csv('{}/summary_out_domain.{}.{}.csv'.format(checkpoint_dir, task, metric))
-            pprint(tmp_df)
+    tmp_df = pd.DataFrame(f1_out_domain['es']).T
+    tmp_df = tmp_df[data]
+    all_data = data + ["all_english"]
+    tmp_df = tmp_df.T[all_data].T
+    pprint(tmp_df)
+    tmp_df.to_csv('{}/summary_out_domain.f1.span.csv'.format(checkpoint_dir))
+    input()
+    tmp_df = pd.DataFrame(f1_out_domain['ner']).T
+    tmp_df = tmp_df.T['all_english']
+    pprint(tmp_df)
+    tmp_df.to_csv('{}/summary_out_domain.f1.csv'.format(checkpoint_dir))
 
 
 if __name__ == '__main__':
-    summary(True, False)
-    summary(True, True)
-    # summary(False, True)
-    # summary(False, False)
+    summary(False, True)
+    summary(False, False)
+    # summary(True, False)
+    # summary(True, True)
