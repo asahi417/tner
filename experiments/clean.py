0c2c471e219371a18af3fd86a08ce3ea4d11e12c
asahi417 <asahi1992ushio@gmail.com>
2021-01-09 18:32:09 +0000

    fix other bugs and refactoring the code

--- experiments/clean.py
+++ experiments/clean.py
@@ -3,18 +3,26 @@ from glob import glob
 
 ckpt = './ckpt'
 for i in glob('{}/*/*/*_ignore.json'.format(ckpt)):
-    print(i)
     os.rename(i, i.replace('_ignore', '_span'))
 
-# for i in glob('{}/*/*'):
-#     data_name = i.split('/')[-1]
-#     for _i in glob('{}/test*.json'.format(i)):
-#         if data_name in _i:
-#             continue
-#         if 'all_english' in _i:
-#             continue
-#
-#         if '_span' not in _i:
-#             os.remove(_i)
+for i in glob('{}/*/*'.format(ckpt)):
+    data_name = i.split('/')[-1]
+    is_lower = '_lower' in data_name
+    print(i, is_lower)
+    input()
+    for _i in glob('{}/test*.json'.format(i)):
+        if data_name in _i:
+            continue
+        if 'all_english' in _i:
+            continue
+
+        if '_span' not in _i:
+            os.remove(_i)
+
+        if '_lower' not in _i and is_lower:
+            os.remove(_i)
+
+        if '_lower' in _i and not is_lower:
+            os.remove(_i)
 
 
