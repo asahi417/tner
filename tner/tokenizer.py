83d248785d9646aa492122e42c0f86673f46750c
asahi417 <asahi1992ushio@gmail.com>
2021-12-01 18:00:47 +0000

    fix

--- tner/tokenizer.py
+++ tner/tokenizer.py
@@ -94,6 +94,7 @@ class TokenizerFixed:
                         else:
                             entity = '-'.join(self.id2label[label].split('-')[1:])
                             fixed_labels += [self.label2id['I-{}'.format(entity)]] * (sub_length - 1)
+                print(word, label, fixed_labels[-1])
             tmp_padding = PAD_TOKEN_LABEL_ID if mask_by_padding_token else self.pad_ids['labels']
             fixed_labels = [tmp_padding] * len(self.sp_token_start['input_ids']) + fixed_labels
             fixed_labels = fixed_labels[:min(len(fixed_labels), max_seq_length - len(self.sp_token_end['input_ids']))]
