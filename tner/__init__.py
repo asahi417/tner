da5dc6c60c09a65422c5901712d58ff791e23f1c
asahi417 <asahi1992ushio@gmail.com>
2022-08-06 16:29:25 +0100

    fix dependency

--- tner/__init__.py
+++ tner/__init__.py
@@ -1,5 +1,4 @@
 from .get_dataset import get_dataset, get_shared_label, concat_dataset
-from .tokenizer_ja import TokenizerJA
 from .ner_model import TransformersNER
 from .ner_tokenizer import NERTokenizer
 from .ner_trainer import Trainer, GridSearcher
